74b68cfd5f61ec727b5d44e1767c5d85a7987e1d
Salman Zulkarnain <salmanzulkarnain123@gmail.com>
2025-01-29 21:50:18 +0700

    tambah index

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Utama</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<div class="say-hello">
+        <h1>Halo, Selamat Datang di Pencatatan Pulsa</h1>
+    </div>
+    <div class="navbar">
+        <ul>
+            <li><a href="user/index.php">Pengguna</a></li>
+            <li><a href="nomor/index.php">Nomor</a></li>
+            <li><a href="paket/index.php">Paket</a></li>
+        </ul>
+    </div>
+</body>
+
+</html>
